da02d02b4e6263f5875f0b1cc8b3a72e21a18347
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2025-10-23 00:14:52 -0300

    Refactor: Move player direction constants and update gitignore for venv path.

--- source/game/player.cpp
+++ source/game/player.cpp
@@ -34,22 +34,25 @@ using	::std::string;
 using	color		=	::tui::terminal::color;
 
 
+const point player::up{ 0, -1 };
+const point player::down{ 0, 1 };
+const point player::left{ -1, 0 };
+const point player::right{ 1, 0 };
+
+
 player::player( const point& box_size, const point& position, const string& character )
 	:m_position{ position }
 	,m_character{ character }
 	,m_box_size{ box_size }
 { }
 
-
 player::~player( ) noexcept { }
-
 
 auto player::draw( terminal& terminal ) const noexcept -> void
 {
 	terminal.set_color( color::white );
 	terminal.print( m_position[ 0 ], m_position[ 1 ], m_character );
 }
-
 
 auto player::move( const point& direction ) -> void
 {
@@ -58,15 +61,8 @@ auto player::move( const point& direction ) -> void
 	m_position %= m_box_size;
 }
 
-
 auto player::get_position( ) const noexcept -> const point& { return m_position; }
 auto player::get_character( ) const noexcept -> const string& { return m_character; }
-
-
-const point player::up{ 0, -1 };
-const point player::down{ 0, 1 };
-const point player::left{ -1, 0 };
-const point player::right{ 1, 0 };
 
 } 
 
