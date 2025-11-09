87147a2e70c4a8dfcc391d674ffe0836edee4e25
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2025-11-09 08:50:14 -0300

    feat(game): Create abstract class game::renderer::drawable
    This commit introduces the new abstract class .
    - It defines a pure virtual method  for collision detection.
    - It includes a protected constructor that initializes a private  member of type .
    - The class uses  as defined in  to adhere to project conventions.
    - A virtual destructor is also defined.

---
+++ source/game/renderer/drawable.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2025 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/* 
+ * File:   game/renderer/drawable.cpp
+ * Author: Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+ * 
+ * Created on 2025-11-09 08:43
+ */
+
+
+
+#include <game/renderer/drawable.hpp>
+
+
+namespace game {
+namespace renderer {
+
+
+using	::std::string;
+
+
+drawable::drawable( const point& p_position ) noexcept
+:
+	m_position{ p_position }
+{ };
+
+
+} } 
+
+
+
