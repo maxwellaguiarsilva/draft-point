0c56ab0fd903b043a084ca583299fedefcba8b9c
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2025-11-27 23:57:29 -0300

    feat: add c++23 features to generic_dispatcher test

--- tests/adhoc/0003_generic_dispatcher/0003_generic_dispatcher.cpp
+++ tests/adhoc/0003_generic_dispatcher/0003_generic_dispatcher.cpp
@@ -1,10 +1,10 @@
 /*
- * Copyright (C) 2025 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+ * Copyright ( C ) 2025 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * ( at your option ) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -22,83 +22,93 @@
  */
 
 
-#include <iostream>
+#include <print>
 #include <vector>
 #include <algorithm>
 #include <string>
 #include <memory>
 
 
-template <typename listener_interface_type>
-class event_dispatcher {
+using	::std::vector;
+using	::std::string;
+using	::std::println;
+using	::std::print;
+using	::std::shared_ptr;
+using	::std::weak_ptr;
+using	::std::remove_if;
+using	::std::make_shared;
+
+
+template< typename t_listener >
+class dispatcher
+{
 public:
-    void add_listener(std::shared_ptr<listener_interface_type> listener_instance) {
-        registered_listeners.push_back(listener_instance);
+    void operator +=( shared_ptr<t_listener> instance )
+	{
+        list.push_back( instance );
     }
 
-    void remove_listener(std::shared_ptr<listener_interface_type> listener_instance) {
-        registered_listeners.erase(
-            std::remove_if(registered_listeners.begin(), registered_listeners.end(),
-                [&](const std::weak_ptr<listener_interface_type>& wp) {
-                    auto sp = wp.lock();
-                    return !sp || sp == listener_instance; 
-                }),
-            registered_listeners.end()
-        );
-    }
-
-    // --- CORREÇÃO APLICADA AQUI ---
-    // Usamos MethodArgs para deduzir a assinatura do ponteiro de função
-    // E CallArgs para deduzir os argumentos passados no broadcast
-    template <typename... MethodArgs, typename... CallArgs>
-    void broadcast(void (listener_interface_type::*member_function_pointer)(MethodArgs...), CallArgs&&... arguments) {
-        auto it = registered_listeners.begin();
-        while (it != registered_listeners.end()) {
-            if (auto strong_ptr = it->lock()) {
-                (strong_ptr.get()->*member_function_pointer)(std::forward<CallArgs>(arguments)...);
-                ++it;
+    //	t_method_args para deduzir a assinatura do ponteiro de função
+    //	t_call_args para deduzir os argumentos passados no broadcast
+    template <typename... t_method_args, typename... t_call_args>
+    void broadcast(
+		 void ( t_listener::*member_function_pointer )( t_method_args... )
+		,t_call_args&&... arguments
+	)
+	{
+        auto iterator = list.begin( );
+        while( iterator != list.end( ) )
+		{
+            if( auto strong_ptr = iterator->lock( ) ) {
+                ( strong_ptr.get( )->*member_function_pointer )( ::std::forward<t_call_args>( arguments )... );
+                ++iterator;
             } else {
-                it = registered_listeners.erase(it);
+                iterator = list.erase( iterator );
             }
         }
     }
 
 private:
-    std::vector<std::weak_ptr<listener_interface_type>> registered_listeners;
+    vector< weak_ptr< t_listener > > list;
 };
 
-class button_listener {
+class button_listener
+{
 public:
-    virtual ~button_listener() = default;
-    // Note que a interface pede const std::string&
-    virtual void on_clicked(const std::string& button_name) = 0;
-    virtual void on_hover(int duration) = 0;
+    virtual ~button_listener( ) = default;
+    virtual void on_clicked( const string& button_name ) = 0;
+    virtual void on_hover( int duration ) = 0;
 };
 
-class concrete_button_logger : public button_listener {
+
+class button_logger : public button_listener {
 public:
-    void on_clicked(const std::string& button_name) override {
-        std::cout << "Button clicked: " << button_name << std::endl;
+    void on_clicked( const string& button_name ) override
+	{
+        println( "button clicked: {}", button_name );
     }
-    void on_hover(int duration) override {
-        std::cout << "Hover: " << duration << std::endl;
+    void on_hover( int duration ) override
+	{
+        println( "hover: {}", duration );
     }
 };
 
-int main() {
-    auto my_logger = std::make_shared<concrete_button_logger>();
-    event_dispatcher<button_listener> notifier;
-    
-    notifier.add_listener(my_logger);
 
-    // Agora isso funciona!
-    // O compilador deduz MethodArgs = {const std::string&}
-    // E CallArgs = {std::string}
-    // E sabe que pode passar um para o outro.
-    notifier.broadcast(&button_listener::on_clicked, std::string("btn_start"));
-    
-    // Isso também funciona (int para int)
-    notifier.broadcast(&button_listener::on_hover, 100);
+int main( int argument_count, char* argument_values[ ] )
+{{
+    const vector< string > arguments( argument_values, argument_values + argument_count );
+	//	for( const auto& value : arguments )
+	//		println( "{}", value );
 
-    return 0;
-}
+    auto logger = make_shared<button_logger>( );
+    dispatcher<button_listener> notifier;
+    
+    notifier += logger;
+
+    notifier.broadcast( &button_listener::on_clicked, "btn_start" );
+    notifier.broadcast( &button_listener::on_hover, 100 );
+
+    return	EXIT_SUCCESS;
+}};
+
+
