30f613c5bcb732216ee3d43cfecc79e3a18d61e9
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2025-12-19 18:26:11 -0300

    feat: usa conceitos em tupled

--- tests/adhoc/0004_templates/0004_templates.cpp
+++ tests/adhoc/0004_templates/0004_templates.cpp
@@ -23,33 +23,45 @@
 
 
 
+//	--------------------------------------------------
 #include <print>
 #include <string>
-#include <tuple>
+#include <vector>
 #include <array>
 #include <ranges>
-#include <utility>
 #include <algorithm>
 #include <functional>
+#include <cstdlib>
 
-
-using	::std::string;
-using	::std::vector;
+#include <tuple>
+#include <utility>
+#include <concepts>
+#include <type_traits>
 //	--------------------------------------------------
+
+
 using	::std::apply;
-using	::std::array;
-using	::std::println;
-using	::std::ranges::all_of;
-using	::std::views::zip;
+using	::std::is_class_v;
+using	::std::is_function_v;
+using	::std::remove_cvref_t;
+using	::std::remove_pointer_t;
+using	::std::tuple_size;
 
 
-//	faça o concept invocable e também o que verifica se t_tuple é uma tupla
+template< typename t_invocable >
+concept invocable	=
+		is_class_v< remove_cvref_t< t_invocable > >
+	or	is_function_v< remove_pointer_t< remove_cvref_t< t_invocable > > >
+;
+template< typename t_tuple >
+concept is_tuple = requires { typename tuple_size< remove_cvref_t< t_tuple > >::type; };
+
 
 template< invocable t_invocable >
 struct tupled
 {
     t_invocable m_function;
-    template< typename t_tuple >
+    template< is_tuple t_tuple >
     inline constexpr auto operator()( t_tuple&& args ) const { return apply( m_function, ::std::forward< t_tuple >( args ) ); }
 };
 
@@ -63,15 +75,25 @@ inline constexpr auto less_equal = tupled{ ::std::less_equal{} };
 
 auto main( const int argument_count, const char* argument_values[ ] ) -> int
 {{
+	using	::std::string;
+	using	::std::vector;
+	using	::std::array;
+	using	::std::println;
+	using	::std::make_pair;
+	using	::std::ranges::all_of;
+	using	::std::views::zip;
+	
 	const vector< string > arguments( argument_values, argument_values + argument_count );
 	for( const auto& value : arguments )
 		println( "{}", value );
-
+	
 	array left	=	{ 1, 2, 3, 4, 5 };
 	array right	=	{ 2, 3, 4, 5, 6 };
-
+	
 	println( "left < right: {}", all_of( zip( left, right ), less_equal ) );
-
+    println( "pair (1, 2) less_equal: {}", less_equal( make_pair( 1, 2 ) ) );
+    println( "array [5, 5] less_equal: {}", less_equal( array{ 5, 5 } ) );
+	
     return	EXIT_SUCCESS;
 }};
 
