0b05a931176d4412bc2b4ba5860a06894c5da3db
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2025-12-21 18:03:13 -0300

    feat: direction name

--- include/game/direction.hpp
+++ include/game/direction.hpp
@@ -29,12 +29,14 @@
 
 #include <sak/sak.hpp>
 #include <string>
+#include <unordered_map>
 
 
 namespace game {
 namespace direction {
 
 using	::std::string;
+using	::std::unordered_map;
 
 
 enum class direction
@@ -47,13 +49,36 @@ enum class direction
 	,up_right
 	,down_left
 	,down_right
-	;
-}
+	,none
+};
+
+
+struct __use_direction
+{
+	constexpr auto operator ( )( direction a_direction ) const -> const string&
+	{
+		static const unordered_map< direction, string > s_directions =
+		{
+			 {	direction::up			,"up"			}
+			,{	direction::down			,"down"			}
+			,{	direction::left			,"left"			}
+			,{	direction::right		,"right"		}
+			,{	direction::up_left		,"up_left"		}
+			,{	direction::up_right		,"up_right"		}
+			,{	direction::down_left	,"down_left"	}
+			,{	direction::down_right	,"down_right"	}
+		};
+
+		static const string s_none	=	"none";
+
+		const auto name = s_directions.find( a_direction );
+		return	( name not_eq s_directions.end( ) ) ? name->second : s_none;
+	}
+};
+inline constexpr auto use_direction	=	__use_direction{ };
 
 
 } } 
 
 
 #endif
-
-
