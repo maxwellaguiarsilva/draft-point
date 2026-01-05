bf6c8c50df5723ab01b976ce7c979ab6e4bda871
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2026-01-05 20:32:14 -0300

    feat(sak): add ranges::chunk utility and update check tool documentation

---
+++ include/sak/ranges/chunk.hpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2026 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/* 
+ * File:   sak/ranges/chunk.hpp
+ * Author: Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+ * 
+ * Created on 2026-01-05 20:25
+ */
+
+
+#pragma once
+#ifndef header_guard_555387710
+#define header_guard_555387710
+
+#include <sak/sak.hpp>
+#include <ranges>
+#include <algorithm>
+
+
+namespace sak::ranges {
+
+
+template< ::std::ranges::viewable_range t_range >
+auto chunk( t_range&& range, const ::std::size_t size )
+{
+	return	::std::views::iota( ::std::size_t( 0 ), ( ::std::ranges::size( range ) + size - 1 ) / size )
+		|	::std::views::transform( [ &range, size ]( const ::std::size_t index ) {
+			auto start	=	::std::ranges::begin( range ) + ( index * size );
+			auto end	=	::std::ranges::begin( range ) + ::std::min( ( index + 1 ) * size, ::std::ranges::size( range ) );
+			return	::std::ranges::subrange( start, end );
+		} );
+}
+
+
+}
+
+
+#endif
+
+
---
+++ tests/adhoc/0005_custom_chunk/0005_custom_chunk.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2026 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/* 
+ * File:   0005_custom_chunk.cpp
+ * Author: Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+ * 
+ * Created on 2026-01-05 19:57
+ */
+
+
+#include <print>
+#include <string>
+#include <vector>
+#include <ranges>
+#include <algorithm>
+#include <exception>
+#include <sak/ranges/chunk.hpp>
+#include <sak/ensure.hpp>
+#include <sak/using.hpp>
+//	--------------------------------------------------
+
+
+auto main( const int argument_count, const char* argument_values[ ] ) -> int
+{{
+	__using( ::sak::
+		,exit_success
+		,exit_failure
+		,ensure
+	)
+	__using( ::std::
+		,print
+		,println
+		,string
+		,vector
+		,exception
+	)
+
+	const vector< string > arguments( argument_values, argument_values + argument_count );
+	for( const auto& value : arguments )
+		println( "{}", value );
+
+	try
+	{
+		vector< int > data	=	{ 1, 2, 3, 4, 5, 6 };
+		auto chunks			=	::sak::ranges::chunk( data, 2 );
+		
+		int chunk_count	=	0;
+		for( auto current_chunk : chunks )
+		{
+			for( int element : current_chunk )
+				print( "{} ", element );
+
+			print( "| " );
+			++chunk_count;
+		}
+		println( "" );
+
+		ensure( chunk_count == 3, "should have 3 chunks" );
+
+		println( "all tests for 0005_custom_chunk passed!" );
+	}
+	catch( const exception& error )
+	{
+		println( "test failed: {}", error.what( ) );
+		return	exit_failure;
+	}
+
+	return	exit_success;
+}}
+
+
