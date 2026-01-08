a5413f7af56785358a3da58d41b4a227c02b9a28
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2026-01-08 01:06:20 -0300

    refactor: move sak::ranges::chunk adhoc tests to structured test file

--- tests/sak/ranges/test_sak_ranges_chunk.cpp
+++ tests/sak/ranges/test_sak_ranges_chunk.cpp
@@ -15,23 +15,20 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 /* 
- * File:   adhoc/0005_custom_chunk/0005_custom_chunk.cpp
+ * File:   sak/ranges/test_sak_ranges_chunk.cpp
  * Author: Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
  * 
- * Created on 2026-01-05 20:32
+ * Created on 2026-01-08 01:05
  */
 
 
 #include <print>
 #include <string>
 #include <vector>
-#include <ranges>
-#include <algorithm>
 #include <exception>
 #include <sak/ranges/chunk.hpp>
 #include <sak/ensure.hpp>
 #include <sak/using.hpp>
-//	--------------------------------------------------
 
 
 auto main( const int argument_count, const char* argument_values[ ] ) -> int
@@ -42,21 +39,24 @@ auto main( const int argument_count, const char* argument_values[ ] ) -> int
 		,ensure
 	)
 	__using( ::std::
-		,print
-		,println
 		,string
 		,vector
+		,print
+		,println
 		,exception
 	)
+	using	::sak::ranges::chunk;
 
 	const vector< string > arguments( argument_values, argument_values + argument_count );
 	for( const auto& value : arguments )
 		println( "{}", value );
-
+	
 	try
 	{
+		println( "starting tests for: sak/ranges/chunk..." );
+
 		vector< int > data	=	{ 1, 2, 3, 4, 5, 6 };
-		auto chunks			=	::sak::ranges::chunk( data, 2 );
+		auto chunks			=	chunk( data, 2 );
 		
 		int chunk_count	=	0;
 		for( auto current_chunk : chunks )
@@ -71,7 +71,7 @@ auto main( const int argument_count, const char* argument_values[ ] ) -> int
 
 		ensure( chunk_count == 3, "should have 3 chunks" );
 
-		println( "all tests for 0005_custom_chunk passed!" );
+		println( "all tests for sak/ranges/chunk passed!" );
 	}
 	catch( const exception& error )
 	{
