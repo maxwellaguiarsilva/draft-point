a6b4fb22da2d36430b11083ff206550f073cb4dd
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2026-01-11 08:36:40 -0300

    Add license headers to tools/*.py and fix unsolicited change in agent_statistic.py

--- tools/agent_statistic.py
+++ tools/agent_statistic.py
@@ -1,4 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
+
+#   Copyright (C) 2025 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+#   
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#   
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#   
+#   You should have received a copy of the GNU General License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   
+#   
+#   File:   agent_statistic
+#   Author: Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+#   
+#   Created on 2026-01-10 00:01:07
 
 
 import json
@@ -98,5 +120,3 @@ if __name__ == "__main__":
             print( run_statistic( { } ) )
     except Exception as e:
         print( f"Error: {str( e )}" )
-
-
--- tools/file_generator.py
+++ tools/file_generator.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
 
+
+#   Copyright (C) 2025 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+#   
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#   
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#   
+#   You should have received a copy of the GNU General License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   
+#   
+#   File:   file_generator
+#   Author: Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+#   
+#   Created on 2026-01-04 19:36:49
+
+
 import sys
 import os
 import subprocess
