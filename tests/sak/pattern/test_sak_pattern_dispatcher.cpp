12fa947ef4ff6215e1c3bea061113063bf7265e6
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2026-01-19 17:21:47 -0300

    fix: restored canonical license header in all cpp and hpp files

--- tests/sak/pattern/test_sak_pattern_dispatcher.cpp
+++ tests/sak/pattern/test_sak_pattern_dispatcher.cpp
@@ -1,25 +1,25 @@
-/*
- * Copyright (C) 2025 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-/* 
- * File:   tests/sak/pattern/test_sak_pattern_dispatcher.cpp
- * Author: Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
- * 
- * Created on 2025-12-28 17:07:07
- */
+//	
+//	Copyright (C) 2025 Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+//	
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//	
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//	
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//	
+//	
+//	File:   tests/sak/pattern/test_sak_pattern_dispatcher.cpp
+//	Author: Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
+//	
+//	Created on 2025-12-28 17:07:07
+//
 
 
 #include <print>
