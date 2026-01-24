396e8a478fc848c0c11df2f8031b7a359443ee70
Maxwell Aguiar Silva <maxwellaguiarsilva@gmail.com>
2026-01-24 18:41:45 -0300

    fix: ensure_list

--- tools/lib/common.py
+++ tools/lib/common.py
@@ -38,15 +38,11 @@ def ensure( expression, message ):
 
 
 def all_is_instance( items, cls ):
-    return  all( isinstance( item, cls ) for item in items )
+    return  isinstance( items, cls ) or ( isinstance( items, list ) and all( isinstance( item, cls ) for item in items ) )
 
 def ensure_list( value, cls = None, message = "" ):
-    if value is None:
-        return  [ ]
-    
-    items = value if isinstance( value, list ) else [ value ]
-    ensure( cls is None or all_is_instance( items, cls ), message or f"invalid parameter: must be a {cls.__name__} or a list of {cls.__name__}" )
-    return  items
+    ensure( cls is None or all_is_instance( value, cls ), message or f"invalid parameter: must be a {cls.__name__} or a list of {cls.__name__}" )
+    return  value if isinstance( value, list ) else [ value ]
 
 
 def get_tool_metadata( action ):
